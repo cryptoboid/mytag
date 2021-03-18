1dbb7aedbd9933a50a3a6879d08d623ce8c9359f
cryptoboid <cryptoboid@protonmail.com>
2021-03-18 03:19:37 -0300

    store detections in local storage!

--- models/ObjectDetector.js
+++ models/ObjectDetector.js
@@ -8,32 +8,34 @@ import * as jpeg from 'jpeg-js'
 import * as FileSystem from 'expo-file-system';
 
 import PredictedImage from "./PredictedImage";
+import DetectionStorage from "../persistence/DetectionStorage";
 
 export default class ObjectDetector {
     constructor() {
         this.model = null;
         this.backendReady = false;
-        this.DETECTION_CACHE = {};
+        this.DETECTION_CACHE = new DetectionStorage();
         this.initBackend();
     }
 
     async initBackend() {
-      await tf.ready();
-      console.log("Finished loading TF");
-      
-      this.model = await cocossd.load();
-      console.log("Finished loading COCO");
+        await tf.ready();
+        console.log("Finished loading TF");
 
-      this.backendReady = true;
+        this.model = await cocossd.load();
+        console.log("Finished loading COCO");
+
+        this.backendReady = true;
     }
 
     _isDetectorReady() {
-      return this.backendReady && this.model;
+        return this.backendReady && this.model;
     }
 
     async classifyImage(image) {
-        if (this.DETECTION_CACHE[image.uri] != null) {
-          return this.DETECTION_CACHE[image.uri];
+        let cachedPreds = await this.DETECTION_CACHE.getPredictions(image.uri);
+        if (cachedPreds) {
+            return new PredictedImage(image, cachedPreds);
         }
 
         if (!this._isDetectorReady()) return {};
@@ -42,7 +44,7 @@ export default class ObjectDetector {
         const predictions = await this.model.detect(imageTensor);
 
         // console.log("PRED", predictions);
-        this.DETECTION_CACHE[image.uri] = predictions;
+        this.DETECTION_CACHE.savePredictions(image.uri, predictions);
 
         let result = new PredictedImage(image, predictions);
         return result;
@@ -54,28 +56,38 @@ async function _uriToTensor(uri) {
 
     const manipResponse = await ImageManipulator.manipulateAsync(
         uri,
-        [{ resize: { width: 400 } }],
-        { compress: 1, format: ImageManipulator.SaveFormat.JPEG }
-      );
-        
+        [{
+            resize: {
+                width: 400
+            }
+        }], {
+            compress: 1,
+            format: ImageManipulator.SaveFormat.JPEG
+        }
+    );
+
     const imgB64 = await FileSystem.readAsStringAsync(manipResponse.uri, {
         encoding: FileSystem.EncodingType.Base64,
-      });
+    });
 
     const imgBuffer = tf.util.encodeString(imgB64, 'base64').buffer;
     const rawImageData = new Uint8Array(imgBuffer);
 
     const TO_UINT8ARRAY = true
-    const { width, height, data } = jpeg.decode(rawImageData, TO_UINT8ARRAY)
+    const {
+        width,
+        height,
+        data
+    } = jpeg.decode(rawImageData, TO_UINT8ARRAY)
     // Drop the alpha channel info for mobilenet
     const buffer = new Uint8Array(width * height * 3)
     let offset = 0 // offset into original data
     for (let i = 0; i < buffer.length; i += 3) {
-      buffer[i] = data[offset]
-      buffer[i + 1] = data[offset + 1]
-      buffer[i + 2] = data[offset + 2]
+        buffer[i] = data[offset]
+        buffer[i + 1] = data[offset + 1]
+        buffer[i + 2] = data[offset + 2]
 
-      offset += 4
+        offset += 4
     }
 
     // this.setState( {imgWidth: width, imgHeight: height} )
